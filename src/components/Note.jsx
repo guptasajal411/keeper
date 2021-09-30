af423d79137be99596345ef95eae56228edfb7bb
Sajal Gupta <guptasajal411@gmail.com>
2021-09-30 12:52:31 +0530

    Map notes array inside Note component

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import notes from '../notes'
 import "./styles.css";
 
 function Note(){
     return(
-        <div className="note">
-            <h1>This is the note title</h1>
-            <p>This is the note content</p>
+        <div>
+            {notes.map((note) => <div className="note">
+                <h1>{note.title}</h1>
+                <p>{note.content}</p>
+            </div>)}
         </div>
     );
 }
